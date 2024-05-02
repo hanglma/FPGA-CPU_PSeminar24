424b9534d507626fe55f8c3aba85470eaede5ad5
hanglma <martin.hangl@jhg-traunreut.de>
2024-05-02 12:39:44 +0200

    correct: mux4

--- codeLibrary/4bitMultiplexer.v
+++ codeLibrary/4bitMultiplexer.v
@@ -3,15 +3,15 @@ module mux4(
   input b,
   input c,
   input d,
-  input [0;1] sel,
+  input [0:1] sel,
   output reg o
   );
 always @ (*) begin
   case (sel)
-    0: o <= A;
-	1: o <= B;
-	2: o <= C;
-	3: o <= D;
+    0: o <= a;
+	  1: o <= b;
+	  2: o <= c;
+	  3: o <= d;
   endcase
 end
 
